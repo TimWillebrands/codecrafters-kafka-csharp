d6fb4eef779566f290216c3ef4e8cb582309b8d3
Tim Willebrands <mail@timwillebrands.nl>
2024-11-23 22:45:55 +0100

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -4,49 +4,55 @@ using CodecraftersKafka;
 
 Console.WriteLine("Logs from your program will appear here!");
 var server = new TcpListener(IPAddress.Any, 9092);
-var cancellationTokenSource = new CancellationTokenSource();
+var cts = new CancellationTokenSource();
+
+Console.CancelKeyPress += (sender, e) =>
+{
+    e.Cancel = true;
+    cts.Cancel();
+};
 
 try
 {
     server.Start();
-    var socket = server.AcceptSocket();
     Console.WriteLine("Server started. Press Ctrl+C to stop.");
-
-    var i = 1;
-
-    Console.CancelKeyPress += (sender, e) =>
+    
+    while (!cts.Token.IsCancellationRequested)
     {
-        e.Cancel = true;
-        cancellationTokenSource.Cancel();
-    };
+        var socket = await server.AcceptSocketAsync(cts.Token);
 
-    while (!cancellationTokenSource.Token.IsCancellationRequested)
-    {
-        var buffer = new Span<byte>(new byte[1024]);
-        socket.Receive(buffer);
-        var request = KafkaRequest.FromSpan(buffer);
+        await Task.Run(async () =>
+        {
+            while (!cts.Token.IsCancellationRequested)
+            {
+                var buffer = new ArraySegment<byte>(new byte[1024]);
+                await socket.ReceiveAsync(buffer);
+                var request = KafkaRequest.FromSpan(buffer);
 
-        Console.WriteLine($"{i} - Request: {request}");
+                Console.WriteLine($"Request: {request}");
 
-        var isApiVersionSupported = request.Header.ApiKeyVersion is <= 0 or > 4;
+                var isApiVersionUnsupported = request.Header.ApiKeyVersion is <= 0 or > 4;
 
-        var response = isApiVersionSupported
-            ? new KafkaResponse<ApiVersionsBody>(
-                new ResponseHeader(request.Header.CorrelationId),
-                new ApiVersionsBody(ErrorCode.UnsupportedVersion, []))
-            : new KafkaResponse<ApiVersionsBody>(
-                new ResponseHeader(request.Header.CorrelationId),
-                new ApiVersionsBody(ErrorCode.None, [
-                    new ApiVersion(ApiKey.ApiVersions, 1, 4),
-                ]));
+                var response = isApiVersionUnsupported
+                    ? new KafkaResponse<ApiVersionsBody>(
+                        new ResponseHeader(request.Header.CorrelationId),
+                        new ApiVersionsBody(ErrorCode.UnsupportedVersion, []))
+                    : new KafkaResponse<ApiVersionsBody>(
+                        new ResponseHeader(request.Header.CorrelationId),
+                        new ApiVersionsBody(ErrorCode.None, [
+                            new ApiVersion(ApiKey.ApiVersions, 1, 4),
+                        ]));
 
-        Console.WriteLine($"{i} - Response: {response}");
+                Console.WriteLine($"Response: {response}");
 
-        var sendBytes = socket.Send(response.ToSpan());
+                var sendBytes = socket.Send(response.ToSpan());
 
-        Console.WriteLine($"{i} - Response of '{sendBytes}' bytes was sent!");
-        
-        i++;
+                Console.WriteLine($"Response of '{sendBytes}' bytes was sent!");
+            }
+        }).ContinueWith((t) =>
+        {
+            socket.Dispose();
+        });
     }
 }
 finally
