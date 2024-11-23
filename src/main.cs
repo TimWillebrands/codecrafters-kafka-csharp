e36a05b263004626c8d3f0f1378ae2f2613e4427
Tim Willebrands <mail@timwillebrands.nl>
2024-11-23 22:52:58 +0100

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -19,45 +19,47 @@ try
     
     while (!cts.Token.IsCancellationRequested)
     {
-        var socket = await server.AcceptSocketAsync(cts.Token);
-
-        await Task.Run(async () =>
+        var client = await server.AcceptTcpClientAsync(cts.Token);
+        _ = HandleClientAsync(client, cts.Token).ContinueWith(task =>
         {
-            while (!cts.Token.IsCancellationRequested)
-            {
-                var buffer = new ArraySegment<byte>(new byte[1024]);
-                await socket.ReceiveAsync(buffer);
-                var request = KafkaRequest.FromSpan(buffer);
-
-                Console.WriteLine($"Request: {request}");
-
-                var isApiVersionUnsupported = request.Header.ApiKeyVersion is <= 0 or > 4;
-
-                var response = isApiVersionUnsupported
-                    ? new KafkaResponse<ApiVersionsBody>(
-                        new ResponseHeader(request.Header.CorrelationId),
-                        new ApiVersionsBody(ErrorCode.UnsupportedVersion, []))
-                    : new KafkaResponse<ApiVersionsBody>(
-                        new ResponseHeader(request.Header.CorrelationId),
-                        new ApiVersionsBody(ErrorCode.None, [
-                            new ApiVersion(ApiKey.ApiVersions, 1, 4),
-                        ]));
-
-                Console.WriteLine($"Response: {response}");
-
-                var sendBytes = socket.Send(response.ToSpan());
-
-                Console.WriteLine($"Response of '{sendBytes}' bytes was sent!");
-            }
-        }).ContinueWith((t) =>
-        {
-            socket.Dispose();
+            if (task.IsFaulted)
+                Console.WriteLine($"Client error: {task.Exception}");
+            client.Dispose();
         });
     }
 }
 finally
 {
     server.Stop();
-    server.Dispose();
-    Console.WriteLine("Server stopped.");
+}
+static async Task HandleClientAsync(TcpClient client, CancellationToken ct)
+{
+    await using var stream = client.GetStream();
+    var buffer = new byte[1024];
+    
+    while (!ct.IsCancellationRequested)
+    {
+        var received = await stream.ReadAsync(buffer, ct);
+        if (received == 0) break; // Client disconnected
+
+        var request = KafkaRequest.FromSpan(new ArraySegment<byte>(buffer, 0, received));
+        
+        Console.WriteLine($"Request: {request}");
+
+        var isApiVersionUnsupported = request.Header.ApiKeyVersion is <= 0 or > 4;
+
+        var response = isApiVersionUnsupported
+            ? new KafkaResponse<ApiVersionsBody>(
+                new ResponseHeader(request.Header.CorrelationId),
+                new ApiVersionsBody(ErrorCode.UnsupportedVersion, []))
+            : new KafkaResponse<ApiVersionsBody>(
+                new ResponseHeader(request.Header.CorrelationId),
+                new ApiVersionsBody(ErrorCode.None, [
+                    new ApiVersion(ApiKey.ApiVersions, 1, 4),
+                ]));
+
+        Console.WriteLine($"Response: {response}");
+
+        await stream.WriteAsync(response.ToSpan().ToArray(), ct);
+    }
 }
