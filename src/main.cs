5e02b9e310df6f4dc67d11f34f9b8f4c65240e91
Tim Willebrands <mail@timwillebrands.nl>
2024-11-23 13:01:21 +0100

    Buffer was not initialised :(

--- src/main.cs
+++ src/main.cs
@@ -7,12 +7,14 @@ Console.WriteLine("Logs from your program will appear here!");
 var server = new TcpListener(IPAddress.Any, 9092);
 
 server.Start();
-var socket = server.AcceptSocket(); // wait for client
-
-var buffer = new Span<byte>();
-socket.Receive(buffer);
-
-Console.WriteLine($"Request buffer: {string.Join(',', buffer.ToArray())}");
+var socket = server.AcceptSocket();
+var buffer = new Span<byte>(new byte[1024]);
+var bytesRead = socket.Receive(buffer);
+var request = KafkaResponse.FromSpan(buffer);
+// var reader = new BinaryReader(new MemoryStream(buffer, 0, bytesRead));
+// var messageSize = reader.ReadInt32();
+// var correlationId = reader.ReadInt32();
+Console.WriteLine($"request: {requestt }");
 
 var response = new KafkaResponse(0, new Header(7));
 Console.WriteLine($"response: {response}");
